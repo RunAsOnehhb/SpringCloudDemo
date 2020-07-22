86888a4c1114edea08f391a10976982481d3f0e3
RunAsOnehhb <1113472499@qq.com>
2020-07-22 17:45:16 +0800

    SpringCloudDemo

---
+++ producer/src/test/java/com/example/producer/ProducerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.producer;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+
+}
